d6f680da4189dfc4406e924ad36df59a329cae13
Brian Krabec <briankrabec@gmail.com>
2022-02-12 01:24:49 -0600

    Admin and models set up

--- lamichoacana/restaurant/models.py
+++ lamichoacana/restaurant/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    food_name = models.CharField(max_length=200)
+    food_desc = models.CharField(max_length=200)
+    food_price = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.food_name
